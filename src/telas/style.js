219a36fa0bd8ef86aa08eecf1a8f7a550f1f79fe
Ismael Oliveira <ismael6991@live.com>
2023-06-15 22:24:52 -0300

    Expo-google-fonts estilizando o componente

--- src/telas/style.js
+++ src/telas/style.js
@@ -16,4 +16,40 @@ export default StyleSheet.create({
     color: '#FFFFFF',
     paddingTop: 16,
   },
+  cesta: {
+    paddingVertical: 8,
+    paddingHorizontal: 16,
+  },
+  nome: {
+    fontSize: 26,
+    lineHeight: 42,
+    color: '#464646',
+    fontFamily: 'MontserratBold',
+  },
+  fazenda: {
+    flexDirection: 'row',
+    paddingVertical: 12,
+  },
+  farmImg: {
+    width: 32,
+    height: 32,
+    marginRight: 12,
+  },
+  farmName: {
+    fontSize: 16,
+    lineHeight: 26,
+    fontFamily: 'MontserratRegular',
+  },
+  descricao: {
+    color: '#a3a3a3',
+    fontSize: 16,
+    lineHeight: 26,
+  },
+  price: {
+    color: '#2a9f85',
+    fontWeight: 'bold',
+    fontSize: 26,
+    lineHeight: 42,
+    marginTop: 8,
+  },
 })
